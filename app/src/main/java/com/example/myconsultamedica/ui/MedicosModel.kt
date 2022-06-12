698167d500f2f28dba6db352eec86dc708500348
Jaslana Carriel <carrieljalsana@gmail.com>
2022-06-11 23:51:26 -0300

    Salvar medico

---
+++ app/src/main/java/com/example/myconsultamedica/ui/MedicosModel.kt
@@ -0,0 +1,17 @@
+package com.example.myconsultamedica.ui
+
+data class MedicosModel (
+    var medId: String? = null,
+    var medNome: String? = null,
+    var medLoug: String? = null,
+    var medNumCasa: String? = null,
+    var medBairro: String? = null,
+    var medCidade: String? = null,
+    var medEstado: String? = null,
+    var medCep: String? = null,
+    var medFone: String? = null,
+    var medCel: String? = null,
+    var medEmail: String? = null,
+    var medSenha: String? = null,
+    var medSenhaConf: String? = null,
+        )
